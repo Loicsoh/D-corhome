29c3985bdfa922a6fa2fbcedd0600e7a7f1e771e
Loicsoh <www.sohloic132@gmail.com>
2025-02-14 08:01:35 +0100

    modif tamplate

--- src/script.js
+++ src/script.js
@@ -1,3 +1,48 @@
+// script de slider
+ const image = [
+  "/images/template/0bc16e52ee2d2336569d508a7c697e64.jpg",
+  "/images/template/2e53a5ce9664dee4a2177bee61716893.jpg",
+  "/images/template/95b9a7760166dbe7190c301a57fdc2aa.jpg"
+ ];
+ const imgElement = document.getElementById("imag");
+
+ let currentIndex = 0;
+
+ setInterval(() => {
+  imgElement.src = image[currentIndex];
+  currentIndex = (currentIndex + 1) % image.length;
+ }, 9000);
+
+// const images = [
+//   "/images/template/0bc16e52ee2d2336569d508a7c697e64.jpg",
+//   "/images/template/2e53a5ce9664dee4a2177bee61716893.jpg",
+//   "/images/template/95b9a7760166dbe7190c301a57fdc2aa.jpg"
+// ];
+
+// let currentIndex = 0;
+// const carouselImage = document.getElementById("carousel-image");
+// const leftButton = document.getElementById("left");
+// const rightButton = document.getElementById("right");
+
+// setInterval(() => {
+//   function displayImage() {
+//     carouselImage.src = images[currentIndex];
+//   }
+  
+//   leftButton.addEventListener("click", () => {
+//     currentIndex = (currentIndex - 1 + images.length) % images.length;
+//     displayImage();
+//   });
+  
+  
+//   rightButton.addEventListener("click", () => {
+//     currentIndex = (currentIndex + 1 ) % images.length;
+//     displayImage();
+//   });
+//   displayImage();
+// }, 3000);
+
+// script navbar
 const toggleMenuBtn = document.querySelector('#menu-btn');
 const toggleMenuImg = document.querySelector('#menu-btn img');
 const toggleMenu = document.querySelector('#toggled-menu');
@@ -17,3 +62,4 @@ function toggleNav() {
     toggleMenuBtn.setAttribute("aria-expaned", "true");
   }
 }
+
